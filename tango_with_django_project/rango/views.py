558fbede0881e17716aeb41081d92cf60b161123
Kirill <kireku@gmail.com>
2018-10-11 02:00:46 +0300

    Добавил страницу about и убрал кеш

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -7,4 +7,5 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
+    context_dict = {'emmessage': "It's about"}
+    return render(request, 'rango/about.html', context_dict)
