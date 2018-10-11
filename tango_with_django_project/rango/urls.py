569477cd110491d214860bb2cc516fe0cf7eac37
Kirill <kireku@gmail.com>
2018-10-11 13:32:48 +0300

    Добавлена страница категорий

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
-    re_path(r'^about/', views.about, name='about')
+    re_path(r'^about/', views.about, name='about'),
+    re_path(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category')
 ]
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from rango.models import Category
+from rango.models import Category, Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -11,3 +11,15 @@ def index(request):
 def about(request):
     context_dict = {'emmessage': "It's about"}
     return render(request, 'rango/about.html', context_dict)
+
+def category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+    return render(request, 'rango/category.html', context_dict)
