cdf7d805ed05f06e170031c67328c415827725d1
Kirill <kireku@gmail.com>
2018-10-12 16:21:32 +0300

    Переписал счетчик с помощью сессий

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -18,31 +18,36 @@ def index(request):
     # Мы используем функцию COOKIES.get(), чтобы получить cookie с количеством посещений.
     # Если cookie существует, exists, возвращаемое значение преобразуется в целое число.
     # Если cookie не существует, по умолчанию значение преобразуется в нуль.
-    visits = int(request.COOKIES.get('visits', '1'))
-
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
     reset_last_visit_time = False
     
-
-    if 'last_visit' in request.COOKIES:
-        last_visit = request.COOKIES['last_visit']
+    last_visit = request.session.get('last_visit')
+    if last_visit:
         last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
-        if (datetime.now() - last_visit_time).days > 0:
-            visits = visits + 1
+
+        if(datetime.now() - last_visit_time).seconds > 0:
+            visits += 1
             reset_last_visit_time = True
     else:
         reset_last_visit_time = True
-        response = render(request, 'rango/index.html', context_dict)
-
-    context_dict['visits'] = visits
-    response = render(request, 'rango/index.html', context_dict)
 
     if reset_last_visit_time:
-        response.set_cookie('last_visit', datetime.now()) 
-        response.set_cookie('visits', visits)
-    return response
-   
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+    context_dict['visits'] = visits
+    
+    return render(request, 'rango/index.html', context_dict)  
 def about(request):
-    context_dict = {'emmessage': "It's about"}
+    if request.session.get('visits'):
+        visits = request.session.get('visits')
+    else:
+        visits = 0
+
+    context_dict = {'emmessage': "It's about",
+        'visits': visits
+        }
     return render(request, 'rango/about.html', context_dict)
 def category(request, category_name_slug):
     context_dict = {}
