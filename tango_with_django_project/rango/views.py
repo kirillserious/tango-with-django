1755c1772af25012d6c06c5e59e5ace8efed3dc3
Kirill <kireku@gmail.com>
2018-10-12 15:55:40 +0300

    Добавил куки количества посещений

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
@@ -11,7 +13,34 @@ def index(request):
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list,
         'pages': page_list}
-    return render(request, 'rango/index.html', context_dict)
+    
+    # Получаем количество посещений сайта.
+    # Мы используем функцию COOKIES.get(), чтобы получить cookie с количеством посещений.
+    # Если cookie существует, exists, возвращаемое значение преобразуется в целое число.
+    # Если cookie не существует, по умолчанию значение преобразуется в нуль.
+    visits = int(request.COOKIES.get('visits', '1'))
+
+    reset_last_visit_time = False
+    
+
+    if 'last_visit' in request.COOKIES:
+        last_visit = request.COOKIES['last_visit']
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+        if (datetime.now() - last_visit_time).days > 0:
+            visits = visits + 1
+            reset_last_visit_time = True
+    else:
+        reset_last_visit_time = True
+        response = render(request, 'rango/index.html', context_dict)
+
+    context_dict['visits'] = visits
+    response = render(request, 'rango/index.html', context_dict)
+
+    if reset_last_visit_time:
+        response.set_cookie('last_visit', datetime.now()) 
+        response.set_cookie('visits', visits)
+    return response
+   
 def about(request):
     context_dict = {'emmessage': "It's about"}
     return render(request, 'rango/about.html', context_dict)
@@ -81,6 +110,7 @@ def add_page(request, category_name_slug):
     return render(request, 'rango/add_page.html', context_dict)
 
 def register(request):
+
     # Логическое значение указывающее шаблону прошла ли регистрация успешно.
     # В начале ему присвоено значение False. Код изменяет значение на True, если регистрация прошла успешно.
     registered = False
